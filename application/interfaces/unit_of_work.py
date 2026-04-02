4f19a54691b54638371129e40f11f0de9b4702dc
andreivanghel <andreivanghel@gmail.com>
2026-04-02 23:31:27 +0200

    Unit of work (interface + Django implementation)

---
+++ application/interfaces/unit_of_work.py
@@ -0,0 +1,20 @@
+from abc import ABC, abstractmethod
+
+
+class IUnitOfWork(ABC):
+
+    @abstractmethod
+    def __enter__(self): 
+        pass
+
+    @abstractmethod
+    def __exit__(self, exc_type, exc_val, exc_tb): 
+        pass
+
+    @abstractmethod
+    def commit(self) -> None: 
+        pass
+
+    @abstractmethod
+    def rollback(self) -> None: 
+        pass
